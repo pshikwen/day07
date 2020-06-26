44d54df06447322bd4249cca19f4b2065d16bc69
Pfumelo Shikweni <pshikwen@c4r13s8.wethinkcode.co.za>
2020-06-26 11:55:07 +0200

    done

---
+++ ex00/ft_strdup.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+int		ft_strlen(char *c)
+{
+	int i;
+
+	i = 0;
+	while (c[i] != '\0')
+		++i;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	char *dest;
+
+	dest = malloc((ft_strlen(src) + 1) * sizeof(char));
+	return (dest ? ft_strcpy(dest, src) : dest);
+}
